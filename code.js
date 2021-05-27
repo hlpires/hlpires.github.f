b9f317efcb418a8796635a30f1e09e67e968e27f
hlpires <higorlpires@gmail.com>
2021-05-26 22:31:56 -0300

    commit

--- code.js
+++ code.js
@@ -4,20 +4,12 @@
 
 
 window.addEventListener('scroll',function(){
-    this.document.getElementById("background").style.backgroundSize = 160 - + window.pageYOffset/15 + '%'});
+    this.document.getElementById("background").style.backgroundSize = 160 - + window.pageYOffset/20 + '%'});
 
-var $target = $('anime'),
-    animationClass='anime-start'
-
-    function animeScroll(){
-        var documentTop = $(document).scrollTop();
-        $target.each(function(){
-            var itemTop = $(this).offset().top;
-            if(documentTop> itemTop){
-                $(this).addClass(animationClass);
-            }else{
-                $(this).removeClass(animationClass);
-            }
-        })
-    }
-    animeScroll();
+jQuery(document).ready(function(){
+"use strict"
+$('.exp').ripples({
+    dropRadius:10,
+    perturbance:0.01,
+});
+});
