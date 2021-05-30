eb863cd8379df3f3b8051739fd98b250d8e96387
hlpires <higorlpires@gmail.com>
2021-05-30 17:17:15 -0300

    commit

--- code.js
+++ code.js
@@ -2,116 +2,137 @@
 
 
 
-
 (function(){
-window.addEventListener('scroll',function(){
-    this.document.getElementById("background").style.backgroundSize = 160 - + window.pageYOffset/40 + '%'});
-}());
-
-
-
-
-
-jQuery(document).ready(function(){
-"use strict"
-$('.exp').ripples({
-    dropRadius:10,
-    perturbance:0.01,
-});
-});
-
-
-
-
-debounce = function(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-
-(function(){
-	var $target = $('.animeCriative'),
-			animationClass = 'anime-start',
-			offset = $(window).height() * 1/8 ;
-
-	function animeScroll() {
-		var documentTop = $(document).scrollTop();
-		
-
-		$target.each(function(){
-			var itemTop = $(this).offset().top;
-            
-			if (documentTop > itemTop - offset) {
-				$(this).addClass(animationClass);
-			} else {
-				$(this).removeClass(animationClass);
-			}
-		});
-	}
-
-	animeScroll();
-
-	$(document).scroll(debounce(function(){
-		animeScroll();
-	}, 200));
-})();
-
-  function scrollwhere() {
-	  
-	$('.skill-per').each(function(){
-		var $this = $(this);
-		var per = $this.attr('per');
-		$this.css("width",per+'%');
-		$({animatedValue: 0}).animate({animatedValue: per},{
-		  duration: 3000,
-		  step: function(){
-			$this.attr('per', Math.floor(this.animatedValue) + '%');
-		  },
-		  complete: function(){
-			$this.attr('per', Math.floor(this.animatedValue) + '%');
-		  }
-		});
-	  });
+	window.addEventListener('scroll',function(){
+		this.document.getElementById("background").style.backgroundSize = 160 - + window.pageYOffset/40 + '%'});
+	}());
 	
-	}	
-
-//desenrolei aq slk keepo//
+	
+	
+	
+	
+	jQuery(document).ready(function(){
+	"use strict"
+	$('.exp').ripples({
+		dropRadius:10,
+		perturbance:0.01,
+	});
+	});
+	
+	
+	
+	
+	debounce = function(func, wait, immediate) {
+		var timeout;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	};
+	
+	
 	(function(){
-		var createClock = setInterval(init, 2000);	
-function init(){
-var offset = $(window).height() * 1/6 + 300;
-console.log(offset + '  offset');
-var scrollwhereV =$(document).scrollTop();
-console.log(scrollwhereV + 'scroll');
-
-
-
-if(scrollwhereV>offset){
-		scrollwhere();
-		console.log("STOP INTERVAL EXECUTED");
-		clearInterval(createClock);
+		var $target = $('.animeCriative'),
+				animationClass = 'anime-start',
+				offset = $(window).height() * 1/8;
+	
+		function animeScroll() {
+			var documentTop = $(document).scrollTop();
+			
+	
+			$target.each(function(){
+				var itemTop = $('.animeCriative').offset().top;
+				
+				if (documentTop > itemTop - offset) {
+					$('.animeCriative').addClass('anime-start');
+					setTimeout(function delay(){
+					animeScroll2();
+					},800)
+				} else {
+					
+				}
+			});
+		}
 		
-}
-}
-init();
-})();
+			
+		
+		function animeScroll2(){
+			
+			$(".animeInovate").addClass('anime-start1');
+			setTimeout(function delay(){
+				animeScroll3();
+				},800)
+		}
+		
+		
 
-/*frase animmation*/
-
-
-
-	function escurecer(){
-		$("#frase").fadeOut();
+		
+		function animeScroll3(){
+			$(".animeCuriosity").addClass('anime-start2');
+			console.log('animescrolll3 ativado')
+		
+		}
+	
+		animeScroll();
+	
+		$(document).scroll(debounce(function(){
+			animeScroll();
+		}, 200));
+	})();
+	
+	  function scrollwhere() {
+		  
+		$('.skill-per').each(function(){
+			var $this = $(this);
+			var per = $this.attr('per');
+			$this.css("width",per+'%');
+			$({animatedValue: 0}).animate({animatedValue: per},{
+			  duration: 3000,
+			  step: function(){
+				$this.attr('per', Math.floor(this.animatedValue) + '%');
+			  },
+			  complete: function(){
+				$this.attr('per', Math.floor(this.animatedValue) + '%');
+			  }
+			});
+		  });
+		
+		}	
+	
+	//desenrolei aq slk keepo//
+		(function(){
+			var createClock = setInterval(init, 2000);	
+	function init(){
+	var offset = $(window).height() * 1/6 + 300;
+	console.log(offset + '  offset');
+	var scrollwhereV =$(document).scrollTop();
+	console.log(scrollwhereV + 'scroll');
+	
+	
+	
+	if(scrollwhereV>offset){
+			scrollwhere();
+			console.log("STOP INTERVAL EXECUTED");
+			clearInterval(createClock);
+			
 	}
-
-	escurecer();
+	}
+	init();
+	})();
+	
+	/*frase animmation*/
+	
+	
+	
+		function escurecer(){
+			$("#frase").fadeOut();
+		}
+	
+		escurecer();
